45d9d41842ffd9c1d2989db767eab13b4412bc6a
Anaru Herbert <anaruherbert@gmail.com>
2019-03-04 23:12:29 +1000

    feat: storm viewer app 1/2

---
+++ ProjectOne/ProjectOne/ProjectOne_StormViewer/ProjectOne_StormViewer/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ProjectOne_StormViewer
+//
+//  Created by anaru herbert on 4/3/19.
+//  Copyright © 2019 anaru herbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager.default
+        
+        // main bundle always has resource path so it is fine to force unwrap this
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+
+}
+
